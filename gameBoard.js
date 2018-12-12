cf7687f47506bf7c11c65061dba6ed374fed813d
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-12-12 13:54:01 -0800

    Implemented basic game logic

---
+++ gameBoard.js
@@ -0,0 +1,34 @@
+exports.makeBoard() {
+  const makeRow = () => {
+    return [0, 0, 0];
+  }
+  return [makeRow(), makeRow(), makeRow()];
+};
+
+exports.move(playerChar, row, col, board) {
+  if (board[row][col] !== 0) {
+    throw new Error(that space is already taken!)
+  } else {
+    board[row][col] = playerChar;
+  }
+};
+
+exports.detectWin(playerChar, board) {
+  for (let i = 0; i < 3; i += 1) {
+    if (board[i].every(item => item === playerChar)) {
+      return true;
+    }
+    if (board.every(row => row[i] ===  playerChar)) {
+      return true;
+    }
+  }
+  let oneDiag = true;
+  let otherDiag = true;
+  let i;
+  let j;
+  for (i = 0, j = 2; i < 3, j >= 0; i += 1, j -= 1) {
+    oneDiag = board[i][i] === playerChar && oneDiag;
+    otherDiag = board[i][j] === playerChar && otherDiag;
+  }
+  return oneDiag || otherDiag;
+}
